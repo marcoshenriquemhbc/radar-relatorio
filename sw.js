60bdc669b2c1793b6a29696f55e9ec3b0fc5edcf
marcoshenriquemhbc <marcoshenriquemhbc@gmail.com>
2026-09-02 02:41:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'radar-v1';
+const CACHE = 'radar-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', (e) => {
   self.skipWaiting();
